40b5624763d10c8f9959685ff7c223bdd0d7cff3
baselqq <baselqarb@gmail.com>
2023-09-17 00:44:00 +0200

    laravel_rest_api

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Auth\UserAuthController;
+use App\Http\Controllers\EmployeeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/register',  [UserAuthController::class, 'register']);
+Route::post('/login', [UserAuthController::class, 'login']);
+
+Route::apiResource('/employee', EmployeeController::class)->middleware('auth:api');
